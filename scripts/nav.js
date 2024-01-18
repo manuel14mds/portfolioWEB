6c3e12666e34dee1e6eb8354099d8aca930da483
manuel14mds <manuel14mds@gmail.com>
2024-01-18 14:21:29 -0500

    projects scaffolding

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,27 @@
+/* function checkIfTarget() {
+    let currentHash
+    setTimeout(()=>{
+        currentHash = window.location.hash;
+        let targetElement = document.getElementById("nav");
+        console.log('hash',currentHash)
+        console.log('target',targetElement)
+    
+        if (currentHash === "#nav") {
+            // El elemento es el target actual
+            document.body.style.setProperty('--scroll-page', 'hidden')
+        } else {
+            // El elemento no es el target
+            document.body.style.setProperty('--scroll-page', 'scroll')
+        }
+    },500)
+} */
+
+/* 
 const openBtn = document.getElementById('openMenu')
-openBtn.addEventListener('click', ()=>{
-    document.body.style.setProperty('--scroll-page', 'hidden')
+openBtn.addEventListener('click', () => {
+    history.pushState({}, '', '#nav');
 })
 const closeBtn = document.getElementById('closeMenu')
-closeBtn.addEventListener('click', ()=>{
-    document.body.style.setProperty('--scroll-page', 'scroll')
-})
+closeBtn.addEventListener('click', () => {
+    history.replaceState({}, '', window.location.pathname + window.location.search);
+}) */
