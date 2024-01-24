3b309e4ba094d731a7d12510da9c03475c1f3ac3
manuel14mds <manuel14mds@gmail.com>
2024-01-24 13:48:40 -0500

    test nav

--- scripts/nav.js
+++ scripts/nav.js
@@ -6,17 +6,19 @@ const navClose = document.querySelector('.nav_close');
 // Agrega eventos para controlar el desplazamiento
 navHamburger.addEventListener('click', function() {
     document.body.style.overflowY = 'hidden';
+    document.body.classList.add('no-scroll');
 });
 
 navClose.addEventListener('click', function() {
     document.body.style.overflowY = 'scroll';
+    document.body.classList.remove('no-scroll');
 });
 
 // Restablecer el desplazamiento cuando se selecciona una opción del menú
 const navLinks = document.querySelectorAll('.nav_link');
-
 navLinks.forEach(function(link) {
     link.addEventListener('click', function() {
         document.body.style.overflowY = 'scroll';
+        document.body.classList.remove('no-scroll');
     });
 });
