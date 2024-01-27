2d293f9b2534dc2f3281ab04258efce9c22b8d1e
manuel14mds <manuel14mds@gmail.com>
2024-01-27 09:31:49 -0500

    dialogs

--- scripts/modals.js
+++ scripts/modals.js
@@ -4,3 +4,48 @@ const skillsOpenBtn = document.getElementById('skills-dialog-open')
 skillsOpenBtn.addEventListener("click", () => {
     skillsDialog.showModal();
 });
+
+const projectsDialog = document.getElementById('projects-dialog')
+const projectsOpenBtn = document.getElementById('projects-dialog-open')
+
+projectsOpenBtn.addEventListener("click", () => {
+    projectsDialog.showModal();
+});
+
+
+// Project modals
+const dialog_one = document.getElementById('dialogProject-one')
+const projectOne_openBtn = document.getElementById('openDialog-projectOne')
+projectOne_openBtn.addEventListener("click", () => {
+    dialog_one.showModal();
+});
+
+const dialog_two = document.getElementById('dialogProject-two')
+const projectTwo_openBtn = document.getElementById('openDialog-projectTwo')
+projectTwo_openBtn.addEventListener("click", () => {
+    dialog_two.showModal();
+});
+
+const dialog_three = document.getElementById('dialogProject-three')
+const projectThree_openBtn = document.getElementById('openDialog-projectThree')
+projectThree_openBtn.addEventListener("click", () => {
+    dialog_three.showModal();
+});
+
+const dialog_four = document.getElementById('dialogProject-four')
+const projectFour_openBtn = document.getElementById('openDialog-projectFour')
+projectFour_openBtn.addEventListener("click", () => {
+    dialog_four.showModal();
+});
+
+const dialog_five = document.getElementById('dialogProject-five')
+const projectFive_openBtn = document.getElementById('openDialog-projectFive')
+projectFive_openBtn.addEventListener("click", () => {
+    dialog_five.showModal();
+});
+
+const dialog_six = document.getElementById('dialogProject-six')
+const projectSix_openBtn = document.getElementById('openDialog-projectSix')
+projectSix_openBtn.addEventListener("click", () => {
+    dialog_six.showModal();
+});
