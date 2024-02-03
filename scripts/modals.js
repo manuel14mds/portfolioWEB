2c95b99fc2242b5461cd0d0d6715e9ecf274d7fb
manuel14mds <manuel14mds@gmail.com>
2024-02-02 23:03:35 -0500

    nav fixed + scoll-body-y dialog fixed

--- scripts/modals.js
+++ scripts/modals.js
@@ -1,8 +1,39 @@
+// el estado puede ser open o close
+let state = "close"
+
+// código para el el body NO haga scroll-Y
+function noScroll(){
+    document.body.style.overflowY = 'hidden';
+    document.body.classList.add('no-scroll');
+}
+
+// código para el el body  haga scroll-Y
+function scroll(){
+    document.body.style.overflowY = 'scroll';
+    document.body.classList.remove('no-scroll');
+}
+
+// verifica el estado y toma acción
+// el valor de event puede ser open o close
+function stateValidator(event){
+    if(event !== state){
+        if( event === "open" && state === "close"){
+            noScroll()
+            state = "open"
+        }else if( event === "close" && state === "open"){
+            scroll()
+            state = "close"
+        }
+    } 
+}
+
+
 const skillsDialog = document.getElementById('skills-dialog')
 const skillsOpenBtn = document.getElementById('skills-dialog-open')
 
 skillsOpenBtn.addEventListener("click", () => {
     skillsDialog.showModal();
+    stateValidator('open')
 });
 
 const projectsDialog = document.getElementById('projects-dialog')
@@ -10,6 +41,7 @@ const projectsOpenBtn = document.getElementById('projects-dialog-open')
 
 projectsOpenBtn.addEventListener("click", () => {
     projectsDialog.showModal();
+    stateValidator('open')
 });
 
 
@@ -18,34 +50,55 @@ const dialog_one = document.getElementById('dialogProject-one')
 const projectOne_openBtn = document.getElementById('openDialog-projectOne')
 projectOne_openBtn.addEventListener("click", () => {
     dialog_one.showModal();
+    stateValidator('open')
 });
 
 const dialog_two = document.getElementById('dialogProject-two')
 const projectTwo_openBtn = document.getElementById('openDialog-projectTwo')
 projectTwo_openBtn.addEventListener("click", () => {
     dialog_two.showModal();
+    stateValidator('open')
 });
 
 const dialog_three = document.getElementById('dialogProject-three')
 const projectThree_openBtn = document.getElementById('openDialog-projectThree')
 projectThree_openBtn.addEventListener("click", () => {
     dialog_three.showModal();
+    stateValidator('open')
 });
 
 const dialog_four = document.getElementById('dialogProject-four')
 const projectFour_openBtn = document.getElementById('openDialog-projectFour')
 projectFour_openBtn.addEventListener("click", () => {
     dialog_four.showModal();
+    stateValidator('open')
 });
 
 const dialog_five = document.getElementById('dialogProject-five')
 const projectFive_openBtn = document.getElementById('openDialog-projectFive')
 projectFive_openBtn.addEventListener("click", () => {
     dialog_five.showModal();
+    stateValidator('open')
 });
 
 const dialog_six = document.getElementById('dialogProject-six')
 const projectSix_openBtn = document.getElementById('openDialog-projectSix')
 projectSix_openBtn.addEventListener("click", () => {
     dialog_six.showModal();
+    stateValidator('open')
 });
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") {
+        // Espera brevemente para que el estado 'open' de los diálogos se actualice
+        stateValidator('close')
+    }
+});
+
+function closeDialog(id){
+    document.getElementById(id).close()
+    stateValidator('close')
+}
+
+
--- scripts/resume.js
+++ scripts/resume.js
@@ -2,3 +2,8 @@ const seeResumeBtn = document.getElementById('see-resume')
 seeResumeBtn.addEventListener('click', ()=>{
     window.open('./docs/Manuel_Florez-software_developer.pdf', '_blank');
 })
+
+const downloadResumeBtn = document.getElementById('downloadResume')
+downloadResumeBtn.addEventListener('click', ()=> {
+    window.open('./docs/Manuel_Florez-software_developer.pdf', '_blank');
+});
