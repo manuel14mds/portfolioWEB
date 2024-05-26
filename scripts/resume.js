2ed17954e9b97e3c99e3ba1e17d0e27047bc2562
manuel14mds <manuel14mds@gmail.com>
2024-05-26 11:28:47 -0500

    Add new CVs and link the new project Red Nacional Cuidados

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,12 +1,12 @@
 try {
     const seeResumeBtn = document.getElementById('see-resume')
     seeResumeBtn.addEventListener('click', ()=>{
-        window.open('./docs/Manuel_Florez-software_developer.pdf', '_blank');
+        window.open('./docs/ManuelFlorez-software-developer.pdf', '_blank');
     })
     
     const downloadResumeBtn = document.getElementById('downloadResume')
     downloadResumeBtn.addEventListener('click', ()=> {
-        window.open('./docs/Manuel_Florez-software_developer.pdf', '_blank');
+        window.open('./docs/ManuelFlorez-software-developer.pdf', '_blank');
     });
     
 } catch (error) {
@@ -16,12 +16,12 @@ try {
 try {
     const verCV = document.getElementById('ver-CV')
     verCV.addEventListener('click', ()=>{
-        window.open('../docs/Desarrollador_de_Software-Manuel_Florez.pdf', '_blank');
+        window.open('../docs/ManuelFlorez-desarrollador-software.pdf', '_blank');
     })
     
     const descargarCV = document.getElementById('descargarCV')
     descargarCV.addEventListener('click', ()=> {
-        window.open('../docs/Desarrollador_de_Software-Manuel_Florez.pdf', '_blank');
+        window.open('../docs/ManuelFlorez-desarrollador-software.pdf', '_blank');
     });
     
 } catch (error) {
